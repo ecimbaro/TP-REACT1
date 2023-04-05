a1a86e7bb5f2289f6005b34ea956a8d9445b3f8f
FlorIniguez <iniguezflorencia788@gmail.com>
2023-04-05 19:53:06 -0300

    react y bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import  {Button } from 'react-bootstrap';
+
+
+function App() {
+
+  return (
+    <div className="container mt-3">
+
+   {/* Prueba boton booststrap */}
+   <Button variant="primary" type="submit"> Submit</Button>
+
+    </div>
+  )
+}
+
+export default App
