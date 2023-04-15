f2c42fc150678fa48953f40884469c9b39a0f0b5
ecimbaro <ecimba@hotmail.com>
2023-04-15 20:11:44 -0300

    Carrousel

--- src/Componentes/Carrousel.jsx
+++ src/Componentes/Carrousel.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const Carrousel = () => {
-  return (
-      <div>
-          <div id="carouselId" class="carousel slide" data-bs-ride="carousel">
+    return (
+
+
+        
+        <div id="carouselId" class="carousel slide" data-bs-ride="carousel">
             <ol class="carousel-indicators">
                 <li data-bs-target="#carouselId" data-bs-slide-to="0" class="active" aria-current="true" aria-label="First slide"></li>
                 <li data-bs-target="#carouselId" data-bs-slide-to="1" aria-label="Second slide"></li>
@@ -11,13 +13,13 @@ const Carrousel = () => {
             </ol>
             <div class="carousel-inner" role="listbox">
                 <div class="carousel-item active">
-                    <img src="holder.js/900x500/auto/#777:#555/text:First slide" class="w-100 d-block" alt="First slide">
+                    <img src="holder.js/900x500/auto/#777:#555/text:First slide" class="w-100 d-block" alt="First slide" />
                 </div>
                 <div class="carousel-item">
-                    <img src="holder.js/900x500/auto/#666:#444/text:Second slide" class="w-100 d-block" alt="Second slide">
+                    <img src="holder.js/900x500/auto/#666:#444/text:Second slide" class="w-100 d-block" alt="Second slide"/>
                 </div>
                 <div class="carousel-item">
-                    <img src="holder.js/900x500/auto/#666:#444/text:Third slide" class="w-100 d-block" alt="Third slide">
+                    <img src="holder.js/900x500/auto/#666:#444/text:Third slide" class="w-100 d-block" alt="Third slide" />
                 </div>
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselId" data-bs-slide="prev">
@@ -27,10 +29,11 @@ const Carrousel = () => {
             <button class="carousel-control-next" type="button" data-bs-target="#carouselId" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
-                </button>
-          </div>
-    </div>
-  )
+            </button>
+        </div>
+        
+    )
+    
 }
-
-export default Carrousel
+    
+export default Carrousel;
