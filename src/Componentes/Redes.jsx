676fbc52cd98beb30bff3f0b5f03433a4997f9c4
ecimbaro <ecimba@hotmail.com>
2023-04-19 11:17:20 -0300

    Carrousel & Banner Redes

---
+++ src/Componentes/Redes.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./style.css";
+
+const Redes = () => {
+  return (
+    <div className="Redes">
+      <h3>Seguinos a traves de nuestras redes</h3>
+      <section className="Redes-btn">
+        <a href="#" className="bi bi-facebook"></a>
+        <a href="#" className="bi bi-instagram"></a>
+        <a href="#" className="bi bi-twitter"></a>
+      </section>
+    </div>
+  );
+};
+
+export default Redes;
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import Carousel from "./Componentes/Carousel";
+import Redes from './Componentes/Redes'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <>
       <Carousel/>
       <App />
+      <Redes/>
     </>
   </React.StrictMode>
 );
