91d09c8f33779f936cf0284fcba086783891c1c5
NicolasBasalo <117233812+NicolasBasalo@users.noreply.github.com>
2023-04-19 21:48:59 -0300

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import ListaProductos from './components/api/ListaProductos';
 import Footer from './components/Footer/Footer'
 import Carousel from './components/carrousel/Carousel'
+import { Navbar } from './components/Navbar/Navbar';
+
 const App = () => {
   return (
     <div className="container mt-3">
+      <Navbar/>
         <Carousel/>
     <ListaProductos/>
     <Footer/>
