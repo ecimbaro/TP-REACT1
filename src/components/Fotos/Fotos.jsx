c068b904bced97f18ebf6480c101f125f038579f
unknown <Yael>
2023-04-28 17:17:50 -0300

    Instagram

---
+++ src/components/Fotos/Fotos.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "../Componentes/Fotos.css";
+import Cafetera1 from "../assets/img/Cafetera1-1.png"
+import Cafetera2 from "../assets/img/Cafetera2-2.png"
+import Reloj from "../assets/img/Reloj-1.png"
+import Celular from "../assets/img/Celular-1.png";
+
+const infoFotos=[
+    {id:1, linkFoto: Reloj, textoDescriptivo:"Reloj"},
+    {id:2, linkFoto: Cafetera1, textoDescriptivo:"Cafetera"},
+    {id:3, linkFoto: Cafetera2, textoDescriptivo:"Cafetera"},
+    {id:4, linkFoto: Celular, textoDescriptivo:"Celular"}
+]
+
+
+const mapDeInfo= infoFotos.map(infoFoto =>{ return(
+    <div className="contenedor">
+         <img className="fotoTexto" src={infoFoto.linkFoto} alt={infoFoto.textoDescriptivo}/>
+    </div>)})
+
+
+function Fotos(){
+     
+    return(
+    <>
+<div> 
+  {mapDeInfo}
+</div>
+</>
+)}
+
+export default Fotos;
---
+++ src/components/Instagram/Instagram.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "../Componentes/Instagram.css";
+import Fotos from "./Fotos";
+
+
+
+
+function Instagram(props){return(
+<>
+<div className="contenedora">
+    <h1 className="Ig">Ingresá a nuestro instagram: @ReactTech-2023</h1>
+    
+    <div> <Fotos></Fotos>
+    </div>
+</div>
+
+
+
+</>)}
+export default Instagram
