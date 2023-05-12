fc621a93f11b040d42ec6f1e068be84096b26ab6
ecimbaro <ecimba@hotmail.com>
2023-05-12 14:08:17 -0300

    Actualizando proyecto

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,12 @@
 import { RiShoppingCartLine } from "react-icons/ri";
-import { useContext,useState } from "react";
+import { useContext, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import { ProductosContext } from "../Contexto/ContextProducts";
-import ModalCarrito from '../Modal/ModalCarrito'
+import ModalCarrito from "../Modal/ModalCarrito";
 import { NavbarBrand } from "react-bootstrap";
 
 function BarraNav() {
@@ -16,37 +16,38 @@ function BarraNav() {
 
   const handleShowModal = () => {
     setShowModal(true);
-  }
+  };
 
   return (
     <>
-    <Navbar className="barra-nav" collapseOnSelect expand="lg" variant="dark">
-      <Container>
-        <Navbar.Brand as={Link} to="/">
-          React-Tech
-        </Navbar.Brand>
-        <NavbarBrand><ModalCarrito className="carrito-btn" showModal={showModal} setShowModal={setShowModal} /></NavbarBrand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/Productos">
-              Productos
-            </Nav.Link>
-            <Nav.Link as={Link} to="/Ofertas">
-              Ofertas </Nav.Link>
+      <Navbar className="barraNav" collapseOnSelect expand="lg" variant="dark">
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            React-Tech
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ms-auto barraNav-carrito">
+              <Nav.Link as={Link} to="/Productos">
+                Productos
+              </Nav.Link>
+              <Nav.Link as={Link} to="/Ofertas">
+                Ofertas{" "}
+              </Nav.Link>
               <Nav.Link as={Link} to="/Categorias">
-              Categorias
-            </Nav.Link>
-          </Nav>
-        
-        </Navbar.Collapse>
-      </Container>
-    
-      
-      
-    </Navbar>
-    
-     </>
+                Categorias
+              </Nav.Link>
+              <ModalCarrito
+              className="barraNav-btn"
+              showModal={showModal}
+              setShowModal={setShowModal}
+            />
+            </Nav>
+            
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   );
 }
 
